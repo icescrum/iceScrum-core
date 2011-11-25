3f18025c3fdd598eb35e9b5152c6c874b0837270
Vincent Barrier <vincent.barrier@icescrum.com>
2011-11-25 22:16:42 +0100

    update version

--- IcescrumCoreGrailsPlugin.groovy
+++ IcescrumCoreGrailsPlugin.groovy
@@ -58,7 +58,7 @@ import org.icescrum.cache.ReleaseKeyGenerator
 class IcescrumCoreGrailsPlugin {
     def groupId = 'org.icescrum'
     // the plugin version
-    def version = "1.4.9"
+    def version = "1.5-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
