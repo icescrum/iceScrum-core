2fea678873551e2d0b80bc2de435eea2a3d79f1d
Vincent Barrier <vincent.barrier@icescrum.com>
2011-11-25 23:32:38 +0100

    update version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,9 +67,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        compile 'org.icescrum:fluxiable:0.3'
+        compile 'org.icescrum:fluxiable:0.3.1'
         compile ':burning-image:0.5.0'
-        compile 'org.icescrum:icescrum-attachmentable:0.2'
+        compile 'org.icescrum:icescrum-attachmentable:0.2.1'
         compile 'spring:spring-security-core:1.2.3'
         compile 'spring:spring-security-acl:1.1'
         compile ':commentable:0.7.5'
