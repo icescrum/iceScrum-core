6ddb33a751276edf138c9ccc02799ecee210a57f
Vincent Barrrier <barrier.vincent@gmail.com>
2012-06-05 14:21:35 +0200

    hibernate 1.3.9

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -78,5 +78,6 @@ grails.project.dependency.resolution = {
         compile ':maven-publisher:0.8.1'
         compile ':rollback-on-exception:0.1'
         compile ':wikitext:0.1.2'
+        compile ':hibernate:1.3.9'
     }
 }
