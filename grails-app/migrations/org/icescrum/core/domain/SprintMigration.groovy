0907e33a38b16d859bf37a5d6218b84c101eef06
Nicolas Noullet <nnoullet@kagilum.com>
2013-06-28 12:37:32 +0200

    Update version

--- grails-app/migrations/org/icescrum/core/domain/SprintMigration.groovy
+++ grails-app/migrations/org/icescrum/core/domain/SprintMigration.groovy
@@ -17,10 +17,36 @@
  * Authors:
  *
  * Vincent Barrier (vbarrier@kagilum.com)
+ * Nicolas Noullet (nnoullet@kagilum.com)
  */
 package org.icescrum.core.domain
 
 class SprintMigration {
+
+    static SELECT_FANTOM_SPRINTS = """
+        SELECT DISTINCT sprint1.id
+        FROM icescrum2_sprint sprint1, icescrum2_timebox timebox1, icescrum2_sprint sprint2, icescrum2_timebox timebox2
+        WHERE sprint1.parent_release_id = sprint2.parent_release_id
+        AND sprint1.id != sprint2.id
+        AND sprint1.id = timebox1.id
+        AND sprint2.id = timebox2.id
+        AND timebox1.order_number = timebox2.order_number
+        AND NOT EXISTS (
+            SELECT story.id
+            FROM icescrum2_story story
+            WHERE story.parent_sprint_id = sprint1.id)
+        AND NOT EXISTS (
+            SELECT task.id
+            FROM icescrum2_task task
+            WHERE task.backlog_id = sprint1.id)
+        AND sprint1.id != (
+            SELECT MIN(sprint3.id)
+            FROM icescrum2_sprint sprint3, icescrum2_timebox timebox3
+            WHERE sprint3.parent_release_id = sprint1.parent_release_id
+            AND sprint3.id = timebox3.id
+            AND timebox3.order_number = timebox1.order_number)
+        AND sprint1.state = 1
+    """
 
     static migration = {
         changeSet(id:'remove_resource_column_sprint', author:'vbarrier', filePath:filePath) {
@@ -28,6 +54,33 @@ class SprintMigration {
                 columnExists(tableName:"icescrum2_sprint", columnName:"resource")
             }
             dropColumn(tableName:"icescrum2_sprint", columnName:"resource")
+        }
+        changeSet(id:'remove_fantom_sprint_availabilities', author:'vbarrier', filePath:filePath) {
+            preConditions(onFail:"MARK_RAN"){
+                tableExists(tableName:"icescrum2_availability")
+            }
+            sql('DELETE FROM icescrum2_availability WHERE sprint_id IN (' + SELECT_FANTOM_SPRINTS + ')')
+        }
+        changeSet(id:'remove_fantom_sprint_timebox', author:'vbarrier', filePath:filePath) {
+            sql('DELETE FROM icescrum2_timebox WHERE id IN (SELECT * FROM (' + SELECT_FANTOM_SPRINTS + ') AS TEMP)')
+        }
+        changeSet(id:'remove_fantom_sprint_sprints', author:'vbarrier', filePath:filePath) {
+            sql('''
+                DELETE FROM icescrum2_sprint
+                WHERE icescrum2_sprint.state = 1
+                AND NOT EXISTS (
+                    SELECT timebox.id
+                    FROM icescrum2_timebox timebox
+                    WHERE timebox.id = icescrum2_sprint.id)
+                AND NOT EXISTS (
+                    SELECT story.id
+                    FROM icescrum2_story story
+                    WHERE story.parent_sprint_id = icescrum2_sprint.id)
+                AND NOT EXISTS (
+                    SELECT task.id
+                    FROM icescrum2_task task
+                    WHERE task.backlog_id = icescrum2_sprint.id)
+            ''')
         }
     }
 
