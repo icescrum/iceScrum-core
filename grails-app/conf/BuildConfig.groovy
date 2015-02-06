f4041d91d61793b4a07794b2295b9d22de20f12c
vbarrier <vbarrier@kagilum.com>
2015-02-06 15:37:32 +0100

    fix core plugin build

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,6 +25,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.dependency.resolver = "maven"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
