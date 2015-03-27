3cd99d400a0047d02d6c45042127de5245fbf4af
Nicolas Noullet <nnoullet@kagilum.com>
2015-03-27 11:22:26 +0100

    Grails 2.5.0 and updated Java dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Kagilum SAS.
+ * Copyright (c) 2015 Kagilum SAS.
  *
  * This file is part of iceScrum.
  *
@@ -21,71 +21,52 @@
  * Nicolas Noullet (nnoullet@kagilum.com)
  */
 
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolver = "maven"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits("global") {}
+    log "warn"
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
         mavenCentral()
         mavenRepo "http://repo.spring.io/milestone"
         mavenRepo "https://oss.sonatype.org/content/repositories/snapshots/"
         mavenRepo "http://repo.icescrum.org/artifactory/plugins-release/"
         mavenRepo "http://repo.icescrum.org/artifactory/plugins-snapshot/"
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
-
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile "org.atmosphere:atmosphere-runtime:2.2.5", {
-            excludes "slf4j-api"
+        compile 'org.atmosphere:atmosphere-runtime:2.2.6', {
+            excludes 'slf4j-api'
         }
-
         compile('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1') {
-            excludes "commons-logging", "xml-apis", "groovy"
+            excludes 'commons-logging', 'xml-apis', 'groovy'
         }
     }
-
     plugins {
-
-        compile ":atmosphere-meteor:1.0.4"
+        compile ':atmosphere-meteor:1.0.4'
         compile ':spring-security-core:2.0-RC4'
         compile ':spring-security-acl:2.0-RC2'
-        compile ":hd-image-utils:1.1"
-        compile 'org.icescrum:icescrum-attachmentable:1.0.1'
-        compile 'org.icescrum:commentable:1.3'
+        compile ':hd-image-utils:1.1'
         compile ':taggable:1.1.0'
         compile ':jdbc-pool:7.0.47'
         compile ':mail:1.0.7'
         compile ':spring-events:1.2'// TODO Remove
-        compile ':jasper:1.10.0'
+        compile ':jasper:1.11.0'
         compile ':rollback-on-exception:0.1'
         compile ':wikitext:0.1.2'
-        compile ":feeds:1.6"
-        runtime(":hibernate4:4.3.6.1") {
+        compile ':feeds:1.6'
+        runtime(':hibernate4:4.3.8.1') {
             export = false
         }
-        build(":release:3.0.1") {
+        build  (':release:3.1.1') {
             export = false
         }
+        compile 'org.icescrum:icescrum-attachmentable:1.0.1'
+        compile 'org.icescrum:commentable:1.3'
     }
 }
