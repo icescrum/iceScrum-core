0bd047ee5352f535da969fa787227ae5a9af929d
vbarrier <vbarrier@kagilum.com>
2015-07-09 16:40:49 +0200

    display automatically tour when needed

--- grails-app/migrations/org/icescrum/core/domain/preferences/UserPreferencesMigration.groovy
+++ grails-app/migrations/org/icescrum/core/domain/preferences/UserPreferencesMigration.groovy
@@ -80,6 +80,30 @@ class UserPreferencesMigration {
             addNotNullConstraint(tableName:"icescrum2_user_preferences",columnName:'display_whats_new',columnDataType:'BIT')
         }
 
+        changeSet(id:'user_preferences_constraint_displayWelcomeTourAndfullProjectTour', author:'vbarrier', filePath:filePath) {
+            preConditions(onFail:"MARK_RAN"){
+                not{
+                    or {
+                        dbms(type:'mssql')
+                        dbms(type:'oracle')
+                    }
+                }
+            }
+            sql('UPDATE icescrum2_user_preferences set display_welcome_tour = false WHERE display_welcome_tour is NULL')
+            sql('UPDATE icescrum2_user_preferences set display_full_project_tour = false WHERE display_full_project_tour is NULL')
+            addNotNullConstraint(tableName:"icescrum2_user_preferences",columnName:'display_welcome_tour',columnDataType:'BOOLEAN')
+            addNotNullConstraint(tableName:"icescrum2_user_preferences",columnName:'display_full_project_tour',columnDataType:'BOOLEAN')
+        }
+        changeSet(id:'user_preferences_constraint_displayWelcomeTourAndfullProjectTour_mssql', author:'vbarrier', filePath:filePath) {
+            preConditions(onFail:"MARK_RAN"){
+                dbms(type:'mssql')
+            }
+            sql('UPDATE icescrum2_user_preferences set display_welcome_tour = 0 WHERE display_welcome_tour is NULL')
+            sql('UPDATE icescrum2_user_preferences set display_full_project_tour = 0 WHERE display_full_project_tour is NULL')
+            addNotNullConstraint(tableName:"icescrum2_user_preferences",columnName:'display_welcome_tour',columnDataType:'BIT')
+            addNotNullConstraint(tableName:"icescrum2_user_preferences",columnName:'display_full_project_tour',columnDataType:'BIT')
+        }
+
         if (Metadata.current['app.promoteVersion'] == 'true'){
             def version = Metadata.current['app.version'].replaceAll(' ','').replaceAll('#','')
             changeSet(id:'user_preferences_reset_displayWhatsNew_'+version, author:'vbarrier', filePath:filePath) {
