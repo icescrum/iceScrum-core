4ead26c1c9cb3cf8c6be38093e0962522607cd57
msoltani <msoltani@kagilum.com>
2015-08-19 18:01:42 +0200

    add  licence

--- grails-app/domain/org/icescrum/core/domain/Mood.groovy
+++ grails-app/domain/org/icescrum/core/domain/Mood.groovy
@@ -1,9 +1,35 @@
+
+/*
+ * Copyright (c) 2015 iceScrum Technologies.
+ *
+ * This file is part of iceScrum.
+ *
+ * iceScrum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * iceScrum is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with iceScrum.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *
+ * Vincent Barrier (vbarrier@kagilum.com)
+ * Stéphane Maldini (stephane.maldini@icescrum.com)
+ * Manuarii Stein (manuarii.stein@icescrum.com)
+ */
+
+
 package org.icescrum.core.domain
 
 
 class Mood {
     static final int MOOD_GOOD = 1
-    static final int MOOD_AVERAGE = 2
+    static final int MOOD_MEH = 2
     static final int MOOD_BAD = 3
     int moodUser = Mood.MOOD_GOOD
     static belongsTo = [User]
