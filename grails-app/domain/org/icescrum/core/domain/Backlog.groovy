4991f5b525f963023f5d19cb10866bd9e6fe18fd
Nicolas Noullet <nnoullet@kagilum.com>
2015-10-05 19:00:42 +0200

    Improve backlogs

--- grails-app/domain/org/icescrum/core/domain/Backlog.groovy
+++ grails-app/domain/org/icescrum/core/domain/Backlog.groovy
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2015 Kagilum SAS.
+ *
+ * This file is part of iceScrum.
+ *
+ * iceScrum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * iceScrum is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with iceScrum.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *
+ * Vincent Barrier (vbarrier@kagilum.com)
+ * Nicolas Noullet (nnoullet@kagilum.com)
+ */
+
 package org.icescrum.core.domain
 
 import grails.converters.JSON
@@ -11,12 +34,12 @@ class Backlog {
 
     static belongsTo = [
             product: Product,
-            owner: User
+            owner  : User
     ]
 
     static mapping = {
         cache true
-        name (blank:false)
+        name(blank: false)
         table 'icescrum2_backlogs'
     }
 
@@ -28,17 +51,17 @@ class Backlog {
         name(blank: false, maxSize: 200, unique: true)
     }
 
-    def getCount(){
+    def getCount() {
         return Story.search(product, JSON.parse(this.filter), false, true)
     }
 
-    def getStories(){
+    def getStories() {
         return Story.search(product, JSON.parse(this.filter), false)
     }
 
     static namedQueries = {
         findAllByProductAndSharedOrOwner { p, s, u ->
-            product{
+            product {
                 eq 'id', p
             }
             or {
