5b984cbe5ed687c3229b5824207832ecf1cf3aec
Nicolas Noullet <nnoullet@kagilum.com>
2015-11-03 12:58:08 +0100

    Proper ISO date parsing (new format with X pattern in Java 7) Fixes incremental offset and wrong dates

--- src/groovy/org/icescrum/core/utils/ServicesUtils.groovy
+++ src/groovy/org/icescrum/core/utils/ServicesUtils.groovy
@@ -43,20 +43,7 @@ class ServicesUtils {
     }
 
     public static parseDateISO8601(String input) {
-        //NOTE: SimpleDateFormat uses GMT[-+]hh:mm for the TZ which breaks
-        //things a bit.  Before we go on we have to repair this.
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz")
-        //this is zero time so we need to add that TZ indicator for
-        if (input.endsWith("Z")) {
-            input = input.substring(0, input.length() - 1) + "GMT-00:00"
-        } else {
-            int inset = 6
-
-            String s0 = input.substring(0, input.length() - inset)
-            String s1 = input.substring(input.length() - inset, input.length())
-
-            input = s0 + "GMT" + s1
-        }
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX")
         return df.parse(input)
     }
 
