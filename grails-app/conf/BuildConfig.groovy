d759f8e474af4fa85f8d08d789f1780f0fdb2465
Nicolas Noullet <nnoullet@kagilum.com>
2016-07-06 16:03:58 +0200

    Try most recent atmosphere runtime

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,7 +42,7 @@ grails.project.dependency.resolution = {
         mavenRepo "http://jaspersoft.artifactoryonline.com/jaspersoft/third-party-ce-artifacts/" // Because Jasper depends on olap4j which is not available anymore the repositories
     }
     dependencies {
-        compile 'org.atmosphere:atmosphere-runtime:2.4.3', {
+        compile 'org.atmosphere:atmosphere-runtime:2.4.5', {
             excludes 'slf4j-api'
         }
         compile('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2') {
