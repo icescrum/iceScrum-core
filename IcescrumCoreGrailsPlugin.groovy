9c7123c67e0dc6f52ef74d2926f0080f569b6847
Nicolas Noullet <nnoullet@kagilum.com>
2016-11-15 16:29:45 +0100

    Formatting

--- IcescrumCoreGrailsPlugin.groovy
+++ IcescrumCoreGrailsPlugin.groovy
@@ -24,70 +24,52 @@
 
 import com.quirklabs.hdimageutils.HdImageService
 import grails.converters.JSON
+import grails.plugin.springsecurity.SpringSecurityService
+import org.codehaus.groovy.grails.commons.ControllerArtefactHandler
 import org.codehaus.groovy.grails.commons.GrailsClassUtils
+import org.codehaus.groovy.grails.plugins.jasper.JasperExportFormat
+import org.codehaus.groovy.grails.plugins.jasper.JasperReportDef
+import org.codehaus.groovy.grails.plugins.jasper.JasperService
 import org.icescrum.core.cors.CorsFilter
 import org.icescrum.core.event.IceScrumEventPublisher
 import org.icescrum.core.event.IceScrumEventType
 import org.icescrum.core.event.IceScrumListener
+import org.icescrum.core.services.UiDefinitionService
+import org.icescrum.core.support.ApplicationSupport
+import org.icescrum.core.support.ProgressSupport
+import org.icescrum.core.ui.UiDefinitionArtefactHandler
 import org.icescrum.core.utils.JSONIceScrumDomainClassMarshaller
 import org.icescrum.plugins.attachmentable.domain.Attachment
 import org.icescrum.plugins.attachmentable.services.AttachmentableService
-import org.codehaus.groovy.grails.plugins.jasper.JasperReportDef
-import org.codehaus.groovy.grails.plugins.jasper.JasperExportFormat
 import org.springframework.web.servlet.support.RequestContextUtils as RCU
-import grails.plugin.springsecurity.SpringSecurityService
-import org.codehaus.groovy.grails.plugins.jasper.JasperService
-import org.icescrum.core.support.ProgressSupport
-import org.icescrum.core.services.UiDefinitionService
-import org.icescrum.core.ui.UiDefinitionArtefactHandler
-import org.codehaus.groovy.grails.commons.ControllerArtefactHandler
-import org.icescrum.core.support.ApplicationSupport
 
 import javax.servlet.http.HttpServletResponse
 import java.lang.reflect.Method
 
 class IcescrumCoreGrailsPlugin {
     def groupId = 'org.icescrum'
-    // the plugin version
     def version = "1.7-SNAPSHOT"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.5 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
     def artefacts = [new UiDefinitionArtefactHandler()]
-
     def watchedResources = [
-        "file:./grails-app/conf/*UiDefinition.groovy",
-        "file:./plugins/*/grails-app/conf/*UiDefinition.groovy"
+            "file:./grails-app/conf/*UiDefinition.groovy",
+            "file:./plugins/*/grails-app/conf/*UiDefinition.groovy"
     ]
-
     def observe = ['controllers']
     def loadAfter = ['controllers', 'feeds', 'hibernate']
-    def loadBefore = ['autobase', 'grails-atmosphere-meteor']
-
-    // TODO Fill in these fields
+    def loadBefore = ['grails-atmosphere-meteor']
     def author = "iceScrum"
     def authorEmail = "contact@icescrum.org"
     def title = "iceScrum core plugin (include domain / services / taglib)"
-    def description = '''
-    iceScrum core plugin (include domain / services / taglib)
-'''
-
-    // URL to the plugin's documentation
-    def documentation = "http://www.icescrum.org/plugin/icescrum-core"
+    def description = '''iceScrum core plugin (include domain / services / taglib)'''
+    def documentation = "https://www.icescrum.com/documentation"
 
     def doWithWebDescriptor = { xml ->
         if (application.config.icescrum.push.enable) {
             addAtmosphereSessionSupport(xml)
         }
         def cors = application.config.icescrum.cors
-        if (cors.enable){
+        if (cors.enable) {
             addCorsSupport(xml, cors)
         }
     }
@@ -107,21 +89,18 @@ class IcescrumCoreGrailsPlugin {
         JasperService jasperService = ctx.getBean('jasperService')
         UiDefinitionService uiDefinitionService = ctx.getBean('uiDefinitionService')
         uiDefinitionService.loadDefinitions()
-
         application.controllerClasses.each {
             addJasperMethod(it, springSecurityService, jasperService)
-            if (it.logicalPropertyName in controllersWithDownloadAndPreview){
+            if (it.logicalPropertyName in controllersWithDownloadAndPreview) {
                 addDownloadAndPreviewMethods(it, attachmentableService, hdImageService)
             }
         }
-
         application.serviceClasses.each {
             addListenerSupport(it, ctx)
         }
     }
 
     def doWithApplicationContext = { applicationContext ->
-        //For iceScrum internal
         Map properties = application.config?.icescrum?.marshaller
         JSON.registerObjectMarshaller(new JSONIceScrumDomainClassMarshaller(application, false, true, properties), 1)
         applicationContext.bootStrapService.start()
@@ -130,9 +109,7 @@ class IcescrumCoreGrailsPlugin {
     def onChange = { event ->
         UiDefinitionService uiDefinitionService = event.ctx.getBean('uiDefinitionService')
         def type = UiDefinitionArtefactHandler.TYPE
-
-        if (application.isArtefactOfType(type, event.source))
-        {
+        if (application.isArtefactOfType(type, event.source)) {
             def oldClass = application.getArtefact(type, event.source.name)
             application.addArtefact(type, event.source)
             application.getArtefacts(type).each {
@@ -142,15 +119,13 @@ class IcescrumCoreGrailsPlugin {
                 }
             }
             uiDefinitionService.reload()
-        }
-        else if (application.isArtefactOfType(ControllerArtefactHandler.TYPE, event.source))
-        {
+        } else if (application.isArtefactOfType(ControllerArtefactHandler.TYPE, event.source)) {
             def controller = application.getControllerClass(event.source?.name)
             HdImageService hdImageService = event.ctx.getBean('hdImageService')
             AttachmentableService attachmentableService = event.ctx.getBean('attachmentableService')
 
-            if(uiDefinitionService.hasWindowDefinition(controller.logicalPropertyName)) {
-                if (controller.logicalPropertyName in controllersWithDownloadAndPreview){
+            if (uiDefinitionService.hasWindowDefinition(controller.logicalPropertyName)) {
+                if (controller.logicalPropertyName in controllersWithDownloadAndPreview) {
                     addDownloadAndPreviewMethods(controller, attachmentableService, hdImageService)
                 }
             }
@@ -166,51 +141,50 @@ class IcescrumCoreGrailsPlugin {
         event.application.mainContext.uiDefinitionService.reload()
     }
 
-    private addDownloadAndPreviewMethods(clazz, attachmentableService, hdImageService){
+    private addDownloadAndPreviewMethods(clazz, attachmentableService, hdImageService) {
         def mc = clazz.metaClass
         def dynamicActions = [
-            download : { ->
-                Attachment attachment = Attachment.get(params.id as Long)
-                if (attachment) {
-                    if (attachment.url){
-                        redirect(url: "${attachment.url}")
-                        return
-                    }else{
-                        File file = attachmentableService.getFile(attachment)
-
-                        if (file.exists()) {
-                            if (!attachment.previewable){
-                                String filename = attachment.filename
-                                ['Content-disposition': "attachment;filename=\"$filename\"",'Cache-Control': 'private','Pragma': ''].each {k, v ->
-                                    response.setHeader(k, v)
-                                }
-                            }
-                            response.contentType = attachment.contentType
-                            response.outputStream << file.newInputStream()
+                download: { ->
+                    Attachment attachment = Attachment.get(params.id as Long)
+                    if (attachment) {
+                        if (attachment.url) {
+                            redirect(url: "${attachment.url}")
                             return
+                        } else {
+                            File file = attachmentableService.getFile(attachment)
+
+                            if (file.exists()) {
+                                if (!attachment.previewable) {
+                                    String filename = attachment.filename
+                                    ['Content-disposition': "attachment;filename=\"$filename\"", 'Cache-Control': 'private', 'Pragma': ''].each { k, v ->
+                                        response.setHeader(k, v)
+                                    }
+                                }
+                                response.contentType = attachment.contentType
+                                response.outputStream << file.newInputStream()
+                                return
+                            }
                         }
                     }
+                    response.status = HttpServletResponse.SC_NOT_FOUND
+                },
+                preview : {
+                    Attachment attachment = Attachment.get(params.id as Long)
+                    File file = attachmentableService.getFile(attachment)
+                    def thumbnail = new File(file.parentFile.absolutePath + File.separator + attachment.id + '-thumbnail.' + (attachment.ext?.toLowerCase() != 'gif' ? attachment.ext : 'jpg'))
+                    if (!thumbnail.exists()) {
+                        thumbnail.setBytes(hdImageService.scale(file.absolutePath, 40, 40))
+                    }
+                    if (thumbnail.exists()) {
+                        response.contentType = attachment.contentType
+                        response.outputStream << thumbnail.newInputStream()
+                    } else {
+                        render(status: 404)
+                    }
                 }
-                response.status = HttpServletResponse.SC_NOT_FOUND
-            },
-            preview: {
-                Attachment attachment = Attachment.get(params.id as Long)
-                File file = attachmentableService.getFile(attachment)
-                def thumbnail = new File(file.parentFile.absolutePath+File.separator+attachment.id+'-thumbnail.'+(attachment.ext?.toLowerCase() != 'gif'? attachment.ext :'jpg'))
-                if (!thumbnail.exists()){
-                    thumbnail.setBytes(hdImageService.scale(file.absolutePath, 40, 40))
-                }
-                if (thumbnail.exists()){
-                    response.contentType = attachment.contentType
-                    response.outputStream << thumbnail.newInputStream()
-                } else {
-                    render (status: 404)
-                }
-            }
         ]
-
-        dynamicActions.each{ actionName, actionClosure ->
-            mc."${GrailsClassUtils.getGetterName(actionName)}" = {->
+        dynamicActions.each { actionName, actionClosure ->
+            mc."${GrailsClassUtils.getGetterName(actionName)}" = { ->
                 actionClosure.delegate = delegate
                 actionClosure.resolveStrategy = Closure.DELEGATE_FIRST
                 actionClosure
@@ -219,17 +193,17 @@ class IcescrumCoreGrailsPlugin {
         }
     }
 
-    private void addJasperMethod(source, springSecurityService, jasperService){
+    private void addJasperMethod(source, springSecurityService, jasperService) {
         try {
             source.metaClass.renderReport = { String reportName, String format, def data, String outputName = null, def parameters = null ->
                 outputName = (outputName ? outputName.replaceAll("[^\\-a-zA-Z\\s]", "").replaceAll(" ", "") + '-' + reportName : reportName) + '-' + (g.formatDate(formatName: 'is.date.file'))
-                if (!session.progress){
-                     session.progress = new ProgressSupport()
+                if (!session.progress) {
+                    session.progress = new ProgressSupport()
                 }
                 session.progress.updateProgress(50, message(code: 'is.report.processing'))
-                if (parameters){
+                if (parameters) {
                     parameters.SUBREPORT_DIR = "${servletContext.getRealPath('reports/subreports')}/"
-                }else{
+                } else {
                     parameters = [SUBREPORT_DIR: "${servletContext.getRealPath('reports/subreports')}/"]
                 }
                 def reportDef = new JasperReportDef(name: reportName,
@@ -271,7 +245,7 @@ class IcescrumCoreGrailsPlugin {
         }
     }
 
-    private addCorsSupport(def xml, def config){
+    private addCorsSupport(def xml, def config) {
         def contextParam = xml.'context-param'
         contextParam[contextParam.size() - 1] + {
             'filter' {
@@ -284,7 +258,7 @@ class IcescrumCoreGrailsPlugin {
                     }
                 }
                 if (config.headers instanceof Map) {
-                    config.headers.each { k,v ->
+                    config.headers.each { k, v ->
                         'init-param' {
                             'param-name'('header:' + k)
                             'param-value'(v)
@@ -299,14 +273,12 @@ class IcescrumCoreGrailsPlugin {
                 }
             }
         }
-
         def urlPattern = config.url.pattern ?: '/*'
         List list = urlPattern instanceof List ? urlPattern : [urlPattern]
-
         def filter = xml.'filter'
         list.each { pattern ->
             filter[0] + {
-                'filter-mapping'{
+                'filter-mapping' {
                     'filter-name'('cors-headers')
                     'url-pattern'(pattern)
                 }
@@ -319,7 +291,7 @@ class IcescrumCoreGrailsPlugin {
             IceScrumListener listener = method.getAnnotation(IceScrumListener)
             if (listener) {
                 def listenerService = ctx.getBean(serviceGrailsClass.propertyName)
-                def domains =  listener.domain() ? [listener.domain()] : listener.domains()
+                def domains = listener.domain() ? [listener.domain()] : listener.domains()
                 domains.each { domain ->
                     def publisherService = ctx.getBean(domain + 'Service')
                     if (publisherService && publisherService instanceof IceScrumEventPublisher) {
