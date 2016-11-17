9a8799309a037b52a4757dd00dc78c96941edfb7
vbarrier <vbarrier@kagilum.com>
2016-11-17 18:37:30 +0100

    update buildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,7 +43,7 @@ grails.project.dependency.resolution = {
         mavenRepo "https://repo.eclipse.org/content/repositories/mylyn/" // For wikitext
     }
     dependencies {
-        compile 'org.atmosphere:atmosphere-runtime:2.4.5', {
+        compile 'org.atmosphere:atmosphere-runtime:2.4.8', {
             excludes 'slf4j-api'
         }
         compile('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2') {
@@ -54,23 +54,24 @@ grails.project.dependency.resolution = {
         compile 'com.google.guava:guava:15.0'// For wikitext (was previously brought by asset-pipeline but they left the dependency: https://github.com/bertramdev/asset-pipeline/pull/117)
     }
     plugins {
-        compile ':atmosphere-meteor:1.0.5'
-        compile ':spring-security-core:2.0.0'
-        compile ':spring-security-acl:2.0.1'
-        compile ':hd-image-utils:1.1'
-        compile 'org.icescrum:taggable:1.1.3'
-        compile ':jdbc-pool:7.0.47'
-        compile 'org.icescrum:mail:1.0.9' // Forked because of https://github.com/gpc/grails-mail/issues/32 which prevents config change
-        compile ':jasper:1.11.0'
-        compile ':rollback-on-exception:0.1'
-        compile ':feeds:1.6'
-        compile ':cache:1.1.8'
-        compile ':cache-ehcache:1.0.5'
-        runtime ':hibernate4:4.3.10'
+        compile ':atmosphere-meteor:1.0.5'      //no support
+        compile ':spring-security-core:2.0.0'   //spring-security-core:3.1.1
+        compile ':spring-security-acl:2.0.1'    //spring-security-acl:3.0.1
+        compile ':hd-image-utils:1.1'           //no support
+        compile 'org.icescrum:taggable:1.1.3'   //to do
+        compile ':jdbc-pool:7.0.47'             //no support
+        compile 'org.icescrum:mail:1.0.9'       //to do //Forked because of https://github.com/gpc/grails-mail/issues/32 which prevents config change
+        compile ':jasper:1.11.0'                //jasper:v2.0.0.RC1
+        compile ':feeds:1.6'                    //no support
+        compile ':cache:1.1.8'                  //cache:3.0.3
+        compile ':cache-ehcache:1.0.5'          //3.0.0.BUILD-SNAPSHOT
+
+        runtime ':rollback-on-exception:0.2'    //no support
+        runtime ':hibernate4:4.3.10'            //hibernate5
         build  (':release:3.1.2') {
             export = false
         }
-        compile 'org.icescrum:icescrum-attachmentable:1.0.1'
-        compile 'org.icescrum:commentable:1.3'
+        compile 'org.icescrum:icescrum-attachmentable:1.0.1'  //to do
+        compile 'org.icescrum:commentable:1.3'                //to do
     }
 }
