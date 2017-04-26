705025019c70539dc58d99529558e7a3777bdd14
Nicolas Noullet <nnoullet@kagilum.com>
2017-04-26 10:21:13 +0200

    Update attachmentable version -> 1.0.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,26 +51,26 @@ grails.project.dependency.resolution = {
         }
         compile 'org.eclipse.mylyn.docs:org.eclipse.mylyn.wikitext.core:2.8.0-SNAPSHOT' // Cannot use 2.9 or above because it is compiled with Java 8 so it won't work with Java 7
         compile 'org.eclipse.mylyn.docs:org.eclipse.mylyn.wikitext.textile.core:2.8.0-SNAPSHOT'
-        compile 'com.google.guava:guava:15.0'// For wikitext (was previously brought by asset-pipeline but they left the dependency: https://github.com/bertramdev/asset-pipeline/pull/117)
+        compile 'com.google.guava:guava:15.0' // For wikitext (was previously brought by asset-pipeline but they left the dependency: https://github.com/bertramdev/asset-pipeline/pull/117)
     }
     plugins {
-        compile ':atmosphere-meteor:1.0.5'      //no support
-        compile ':spring-security-core:2.0.0'   //spring-security-core:3.1.1
-        compile ':spring-security-acl:2.0.1'    //spring-security-acl:3.0.1
-        compile ':hd-image-utils:1.1'           //no support
-        compile 'org.icescrum:taggable:1.1.3'   //to do
-        compile ':jdbc-pool:7.0.47'             //no support
-        compile 'org.icescrum:mail:1.0.9'       //to do //Forked because of https://github.com/gpc/grails-mail/issues/32 which prevents config change
-        compile ':jasper:1.11.0'                //jasper:v2.0.0.RC1
-        compile ':feeds:1.6'                    //no support
-        compile ':cache:1.1.8'                  //cache:3.0.3
-        compile ':cache-ehcache:1.0.5'          //3.0.0.BUILD-SNAPSHOT
+        compile ':atmosphere-meteor:1.0.5'
+        compile ':spring-security-core:2.0.0'
+        compile ':spring-security-acl:2.0.1'
+        compile ':hd-image-utils:1.1'
+        compile 'org.icescrum:taggable:1.1.3'
+        compile ':jdbc-pool:7.0.47'
+        compile 'org.icescrum:mail:1.0.9' // Forked because of https://github.com/gpc/grails-mail/issues/32 which prevents config change
+        compile ':jasper:1.11.0'
+        compile ':feeds:1.6'
+        compile ':cache:1.1.8'
+        compile ':cache-ehcache:1.0.5'
 
-        runtime ':hibernate4:4.3.10'            //hibernate5
+        runtime ':hibernate4:4.3.10'
         build  (':release:3.1.2') {
             export = false
         }
-        compile 'org.icescrum:icescrum-attachmentable:1.0.2'  //to do
-        compile 'org.icescrum:commentable:1.3'                //to do
+        compile 'org.icescrum:icescrum-attachmentable:1.0.3'
+        compile 'org.icescrum:commentable:1.3'
     }
 }
