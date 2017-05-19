88c9bb0c096c43af54bf3a93b8046d089c6d5123
Nicolas Noullet <nnoullet@kagilum.com>
2017-05-19 14:41:33 +0200

    Fix depends on constraint

--- grails-app/domain/org/icescrum/core/domain/Story.groovy
+++ grails-app/domain/org/icescrum/core/domain/Story.groovy
@@ -103,7 +103,8 @@ class Story extends BacklogElement implements Cloneable, Serializable {
         affectVersion(nullable: true)
         effort(nullable: true, validator: { newEffort, story -> newEffort == null || (newEffort >= 0 && newEffort < 1000) ?: 'invalid' })
         creator(nullable: true) // in case of a user deletion, the story can remain without owner
-        dependsOn(nullable: true, validator: { newDependsOn, story -> newDependsOn == null || (newDependsOn.backlog.id == story.backlog.id && newDependsOn.state >= story.state) ?: 'invalid' })
+        dependsOn(nullable: true, validator: { newDependsOn, story -> newDependsOn == null ||
+                                               (newDependsOn.backlog.id == story.backlog.id && (newDependsOn.state >= story.state || newDependsOn.state == STATE_ACCEPTED && story.state == STATE_ESTIMATED)) ?: 'invalid' })
         origin(nullable: true)
     }
 
