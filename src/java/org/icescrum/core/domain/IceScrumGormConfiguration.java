ddf2eb015879f8d3a4eb0ffadc38dd7a9d216729
Nicolas Noullet <nnoullet@kagilum.com>
2017-10-04 09:59:30 +0200

    T114-0 - Restrict size to 30 chars in foreign key custom constraint name generation

--- src/java/org/icescrum/core/domain/IceScrumGormConfiguration.java
+++ src/java/org/icescrum/core/domain/IceScrumGormConfiguration.java
@@ -2,12 +2,10 @@ package org.icescrum.core.domain;
 
 import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration;
 import org.hibernate.MappingException;
-import org.hibernate.mapping.ForeignKey;
-import org.hibernate.mapping.PersistentClass;
-import org.hibernate.mapping.RootClass;
-import org.hibernate.mapping.UniqueKey;
+import org.hibernate.mapping.*;
 
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class IceScrumGormConfiguration extends GrailsAnnotationConfiguration {
@@ -23,21 +21,53 @@ public class IceScrumGormConfiguration extends GrailsAnnotationConfiguration {
         if (_alreadyProcessed) {
             return;
         }
-        for (PersistentClass pc : (Collection<PersistentClass>) classes.values()) {
+        for (PersistentClass pc : classes.values()) {
             if (pc instanceof RootClass) {
                 RootClass root = (RootClass) pc;
-                for (Iterator iter = root.getTable().getForeignKeyIterator(); iter.hasNext(); ) {
+                Table table = root.getTable();
+                for (Iterator iter = table.getForeignKeyIterator(); iter.hasNext(); ) {
                     ForeignKey fk = (ForeignKey) iter.next();
-                    String tableName = fk.getReferencedTable().getName().replaceAll("_", "");
-                    fk.setName(fk.getName() + tableName);
+                    String fkName;
+                    // Avoid duplicates by adding the reference table name in the constraint name generation proces
+                    // Duplicates happen when FK on timebox because it generates not one but two FK for the same table and column, e.g. one to is_timebox and one to is_release
+                    // Be careful, foreign keys in join table (many to many) don't use this method to generate key names
+                    if ("true".equals(System.getProperty("icescrum.oracle"))) {
+                        System.out.println("Oracle key naming");
+                        // New naming that avoids duplicate while staying under 30 characters
+                        fkName = generateName(table, fk);
+                    } else {
+                        System.out.println("Normal key naming");
+                        // Below is legacy naming that avoids duplicated but exceeds 30 characters
+                        String tableName = fk.getReferencedTable().getName().replaceAll("_", "");
+                        fkName = fk.getName() + tableName;
+                    }
+                    fk.setName(fkName);
                 }
-                UniqueKey nameKey = root.getTable().getUniqueKey("unique_name");
+                UniqueKey nameKey = table.getUniqueKey("unique_name");
                 if (nameKey != null) {
-                    String tableName = root.getTable().getName().replaceAll("_", "");
+                    String tableName = table.getName().replaceAll("_", "");
                     nameKey.setName(nameKey.getName() + tableName);
                 }
             }
         }
         _alreadyProcessed = true;
+    }
+
+    // Copies org.hibernate.mapping.Constraint behavior to stay under 30 chars but adds the reference table name in the hash generation
+    private static String generateName(Table table, ForeignKey fk) {
+        StringBuilder sb = new StringBuilder("table`" + table.getName() + "`");
+        Column[] alphabeticalColumns = (Column[]) fk.getColumns().toArray(new Column[0]);
+        Arrays.sort(alphabeticalColumns, new Comparator<Column>() {
+            @Override
+            public int compare(Column col1, Column col2) {
+                return col1.getName().compareTo(col2.getName());
+            }
+        });
+        for (Column column : alphabeticalColumns) {
+            String columnName = column == null ? "" : column.getName();
+            sb.append("column`" + columnName + "`");
+            sb.append(fk.getReferencedTable().getName()); // This is the customized part that allows to avoid duplicates
+        }
+        return "FK" + Constraint.hashedName(sb.toString());
     }
 }
