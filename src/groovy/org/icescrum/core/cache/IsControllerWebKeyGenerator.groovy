682241a7a451dc9e7f11bfe84224bed79bd8920f
Nicolas Noullet <nnoullet@kagilum.com>
2018-01-15 18:49:06 +0100

    T749 Remove cache logs

--- src/groovy/org/icescrum/core/cache/IsControllerWebKeyGenerator.groovy
+++ src/groovy/org/icescrum/core/cache/IsControllerWebKeyGenerator.groovy
@@ -2,14 +2,11 @@ package org.icescrum.core.cache
 
 import grails.plugin.cache.web.filter.WebKeyGenerator
 import grails.util.Holders
-import org.apache.commons.logging.LogFactory
 import org.codehaus.groovy.grails.web.util.WebUtils
 
 import javax.servlet.http.HttpServletRequest
 
 class IsControllerWebKeyGenerator implements WebKeyGenerator {
-
-    private static final log = LogFactory.getLog(this)
 
     @Override
     String generate(HttpServletRequest request) {
@@ -20,9 +17,6 @@ class IsControllerWebKeyGenerator implements WebKeyGenerator {
         def artefact = Holders.grailsApplication.getArtefactByLogicalPropertyName('Controller', controllerName)
         def controller = Holders.grailsApplication.getMainContext().getBean(artefact.clazz.name)
         def key = controller."${actionName}CacheKey"() + params.inject([]) { result, entry -> result << entry.key + '_' + entry.value }.join('_')
-        if (log.isDebugEnabled()) {
-            log.debug("${request.forwardURI} cache key generated: ${key}")
-        }
         return key
     }
 }
