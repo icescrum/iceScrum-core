7ef386fe176d910182b5bc62d1bbd6c4d3d08573
vbarrier <vbarrier@kagilum.com>
2018-04-06 10:50:17 +0200

    fix atmosphere

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,7 +43,7 @@ grails.project.dependency.resolution = {
         mavenRepo "https://repo.eclipse.org/content/repositories/mylyn/" // For wikitext
     }
     dependencies {
-        compile 'org.atmosphere:atmosphere-runtime:2.4.22', {
+        compile('org.atmosphere:atmosphere-runtime:2.4.23') {
             excludes 'slf4j-api'
         }
         compile('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2') {
