d912a42bfbcc644da446e70586c7586171053c16
Nicolas Noullet <nnoullet@kagilum.com>
2018-04-11 10:11:13 +0200

    T1073-0 Fix admin restart from UI

--- grails-app/services/org/icescrum/core/services/PushService.groovy
+++ grails-app/services/org/icescrum/core/services/PushService.groovy
@@ -165,9 +165,9 @@ class PushService {
     }
 
     public static def buildMessage(String namespace, String eventType, object) {
-        def messageId = object instanceof Map ? object.messageId : namespace + "-" + eventType + "-" + object.id
+        def messageId = object instanceof Map ? object.messageId : namespace + '-' + eventType + '-' + object.id
         def message = [messageId: messageId, namespace: namespace, eventType: eventType, object: object]
-        message.content = (message as JSON).toString()
-        return message // toString() required to serialize eagerly (otherwise error because no session in atmosphere thread)
+        message.content = (message as JSON).toString() // toString() required to serialize eagerly (otherwise error because no session in atmosphere thread)
+        return message
     }
 }
