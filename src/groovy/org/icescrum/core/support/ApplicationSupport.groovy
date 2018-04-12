da487847e916095f9b22be370817ba55c8cc86c4
Nicolas Noullet <nnoullet@kagilum.com>
2018-04-12 10:35:01 +0200

    More debug for server URL

--- src/groovy/org/icescrum/core/support/ApplicationSupport.groovy
+++ src/groovy/org/icescrum/core/support/ApplicationSupport.groovy
@@ -108,7 +108,7 @@ class ApplicationSupport {
             }
             serverUrl += req.contextPath
             if (grailsRequest.params.debugServerURL) {
-                log.debug('URL: \n' + '\tx-forwarded-proto: ' + req.getHeader('x-forwarded-proto') + '\n'
+                log.info('URL: \n' + '\tx-forwarded-proto: ' + req.getHeader('x-forwarded-proto') + '\n'
                         + '\tselected proto: ' + scheme + '\n'
                         + '\tx-forwarded-port: ' + req.getHeader('x-forwarded-port') + '\n'
                         + '\tselected port: ' + port + '\n'
