0bef1d6819aa293f5c061084feb3ed1b7ba2d743
Nicolas Noullet <nnoullet@kagilum.com>
2018-08-10 14:25:31 +0200

    Use the attachment returned by the method rather than picking the latest, as the dateCreated may be the same for two attachments

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -71,7 +71,7 @@ grails.project.dependency.resolution = {
         build(':release:3.1.2') {
             export = false
         }
-        compile 'org.icescrum:icescrum-attachmentable:1.0.3'
+        compile 'org.icescrum:icescrum-attachmentable:1.0.4'
         compile 'org.icescrum:commentable:1.3'
     }
 }
