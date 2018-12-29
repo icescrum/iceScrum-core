930788cb1e1ec4ce70cce9b17e1530e9d1fad180
vbarrier <vbarrier@kagilum.com>
2018-12-29 12:25:11 +0100

    improvement on rest acceptanceTests

--- grails-app/domain/org/icescrum/core/domain/AcceptanceTest.groovy
+++ grails-app/domain/org/icescrum/core/domain/AcceptanceTest.groovy
@@ -80,20 +80,37 @@ class AcceptanceTest implements Serializable {
         results ? results.first() : null
     }
 
-    static List<AcceptanceTest> getAllInProject(projectId) {
+    static List<AcceptanceTest> getAllInProject(projectId, term = '', states = [1, 5, 10]) {
         executeQuery(
                 """SELECT at
                    FROM org.icescrum.core.domain.AcceptanceTest as at
-                   WHERE at.parentStory.backlog.id = :pid """, [pid: projectId])
+                   WHERE at.parentStory.backlog.id = :pid 
+                   AND (at.name LIKE :term OR at.description LIKE :term)
+                   AND at.state in (:states)""",
+                [pid: projectId, term: '%' + term + '%', states: states ?: [1, 5, 10]]
+        )
     }
 
-    static List<AcceptanceTest> getAllInStory(projectId, storyId) {
+    static List<AcceptanceTest> getAllInStory(projectId, storyId, term = '', states = [1, 5, 10]) {
         executeQuery(
                 """SELECT at
                    FROM org.icescrum.core.domain.AcceptanceTest as at
                    WHERE at.parentStory.backlog.id = :pid
-                   AND at.parentStory.id = :sid
-                   ORDER BY at.rank, at.uid""", [sid: storyId, pid: projectId])
+                   AND (at.name LIKE :term OR at.description LIKE :term) 
+                   AND at.parentStory.id = :sid 
+                   AND at.state in (:states)
+                   ORDER BY at.rank, at.uid""", [sid: storyId, pid: projectId, term: '%' + term + '%', states: states ?: [1, 5, 10]])
+    }
+
+    static List<AcceptanceTest> getAllInSprint(projectId, sprintId, term = '', states = [1, 5, 10]) {
+        executeQuery(
+                """SELECT at
+                   FROM org.icescrum.core.domain.AcceptanceTest as at
+                   WHERE at.parentStory.backlog.id = :pid
+                   AND (at.name LIKE :term OR at.description LIKE :term) 
+                   AND at.parentStory.parentSprint.id = :sid
+                   AND at.state in (:states)
+                   ORDER BY at.rank, at.uid""", [sid: sprintId, pid: projectId, term: '%' + term + '%', states: states ?: [1, 5, 10]])
     }
 
     static AcceptanceTest withAcceptanceTest(long projectId, long id) {
