053bb965f929027862ce695305adff9d906924d3
Nicolas Noullet <nnoullet@kagilum.com>
2019-01-02 12:59:28 +0100

    Cleanup

--- grails-app/taglib/org/icescrum/core/taglib/AppTagLib.groovy
+++ grails-app/taglib/org/icescrum/core/taglib/AppTagLib.groovy
@@ -6,6 +6,7 @@ class AppTagLib {
 
     def appService
 
+    // Use only with Apps that need reload
     def ifAppIsEnabledForProject = { attrs, body ->
         assert attrs.app
         if (attrs.project && appService.isEnabledAppForProject(attrs.project, attrs.app)) {
