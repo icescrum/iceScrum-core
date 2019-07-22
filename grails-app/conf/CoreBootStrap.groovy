71bd0e7e830cef0435ff8154c1d4cc603a37fb06
vbarrier <vbarrier@kagilum.com>
2019-07-22 16:07:03 +0200

    fix user creation

--- grails-app/conf/CoreBootStrap.groovy
+++ grails-app/conf/CoreBootStrap.groovy
@@ -32,18 +32,7 @@ class CoreBootStrap {
     def init = { servletContext ->
         if (Environment.current == Environment.DEVELOPMENT && !System.properties['icescrum.noDummyze'] && User.count() <= 1) {
             println "Creating dummy users"
-            userService.save(new User(username: "az", email: "az@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ae", email: "ae@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ad", email: "ad@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ac", email: "ac@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "av", email: "av@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ab", email: "ab@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ah", email: "ah@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ap", email: "ap@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "aa", email: "aa@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "aq", email: "aq@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "aw", email: "aw@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
-            userService.save(new User(username: "ax", email: "ax@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
+            userService.save(new User(username: "a", email: "a@gmail.com", firstName: "Roberto", lastName: 'Doe', password: "a", preferences: new UserPreferences(language: 'en', activity: 'Consultant')))
             userService.save(new User(username: "z", email: "z@gmail.com", firstName: "Bernardo", lastName: 'Doe', password: "z", preferences: new UserPreferences(language: 'en', activity: 'WebDesigner', enableDarkMode: true, menu: ["taskBoard": "1", "planning": "2", "backlog": "3", "feature": "4", "project": "5"])))
         }
     }
